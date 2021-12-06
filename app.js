1baf360ee1fdcfff8dff4ccf3a7cb4e8dcee1e56
Shogo Watanabe <wsg.midday21@gmail.com>
2021-12-07 06:11:27 +0900

    adjust indent

--- app.js
+++ app.js
@@ -9,62 +9,62 @@ const DEFAULT_PARAMS = { on: true, bri: 96, xy: [0.15, 0.05], effect: 'none' }
 const weather = require('./weather')
 
 async function main() {
-    const input = process.argv[2]
+  const input = process.argv[2]
 
-    const commands = {
-        on:      { ...DEFAULT_PARAMS },
-        off:     { ...DEFAULT_PARAMS, on: false },
-        loop:    { ...DEFAULT_PARAMS, effect: 'colorloop' },
-        stop:    { ...DEFAULT_PARAMS, effect: 'none' }, // equal to 'on'
-        random:  { ...DEFAULT_PARAMS, xy: [Math.round(Math.random()*100)/100, Math.round(Math.random()*100)/100] },
-        red:     { ...DEFAULT_PARAMS, xy: [0.69, 0.27] },
-        weather: { ...DEFAULT_PARAMS, xy: await getXyWithWeather() }
-    }
+  const commands = {
+    on:      { ...DEFAULT_PARAMS },
+    off:     { ...DEFAULT_PARAMS, on: false },
+    loop:    { ...DEFAULT_PARAMS, effect: 'colorloop' },
+    stop:    { ...DEFAULT_PARAMS, effect: 'none' }, // equal to 'on'
+    random:  { ...DEFAULT_PARAMS, xy: [Math.round(Math.random()*100)/100, Math.round(Math.random()*100)/100] },
+    red:     { ...DEFAULT_PARAMS, xy: [0.69, 0.27] },
+    weather: { ...DEFAULT_PARAMS, xy: await getXyWithWeather() }
+  }
 
-    // when command is empty or 'help'
-    if (!input || input === 'help') {
-        showCommands(commands)
-        return
-    }
-    // when command exists
-    for (let commandName in commands) {
-        if (input === commandName) {
-            turnHue(commands[commandName])
-            return
-        }
-    }
-    // when command doesn't exist
-    console.log('command not exist' + '\n')
+  // when command is empty or 'help'
+  if (!input || input === 'help') {
     showCommands(commands)
-
     return
+  }
+  // when command exists
+  for (let commandName in commands) {
+    if (input === commandName) {
+      turnHue(commands[commandName])
+      return
+    }
+  }
+  // when command doesn't exist
+  console.log('command not exist' + '\n')
+  showCommands(commands)
+
+  return
 }
 main()
 
 async function turnHue(params = DEFAULT_PARAMS) {
-    axios({
-        url: `http://${IP}/api/${ID}/lights/1/state`,
-        method: 'PUT',
-        headers: { "Content-Type" : 'application/json'},
-        data: params
-    })
-        .then(res => console.log('Huuuuuuuuuue!'))
-        .catch(err => console.log(err.response.data))
+  axios({
+      url: `http://${IP}/api/${ID}/lights/1/state`,
+      method: 'PUT',
+      headers: { "Content-Type" : 'application/json'},
+      data: params
+  })
+    .then(res => console.log('Huuuuuuuuuue!'))
+    .catch(err => console.log(err.response.data))
 }
 
 async function showCommands(commands) {
-    const output =
-        'usage: hue [command]' + '\n' +
-        'available command ↓' + '\n' + '\n' +
-        Object.keys(commands).map(commandName => ` hue ${commandName}`).join('\n')
+  const output =
+      'usage: hue [command]' + '\n' +
+      'available command ↓' + '\n' + '\n' +
+      Object.keys(commands).map(commandName => ` hue ${commandName}`).join('\n')
     console.log(output)
 }
 
 async function getXyWithWeather() {
-    const prec = await weather.getTomorrowPrecipitation()
-    return prec <= 0 ? [0.638, 0.248] :
-        prec <= 30 ? [0.425, 0.467] :
-        prec <= 60 ? [0.313, 0.329] :
-        prec <= 80 ? [0.130, 0.100] :
-        [0.163, 0.026]
+  const prec = await weather.getTomorrowPrecipitation()
+  return prec <= 0 ? [0.638, 0.248] :
+    prec <= 30 ? [0.425, 0.467] :
+    prec <= 60 ? [0.313, 0.329] :
+    prec <= 80 ? [0.130, 0.100] :
+    [0.163, 0.026]
 }
--- weather.js
+++ weather.js
@@ -1,29 +1,29 @@
 const axios = require('axios')
 
 async function getCurrentTemp() {
-    return axios({
-        url: 'https://www.nhk.or.jp/weather-data/v1/lv2/current/?pid=tokyo&akey=18cce8ec1fb2982a4e11dd6b1b3efa36'
+  return axios({
+    url: 'https://www.nhk.or.jp/weather-data/v1/lv2/current/?pid=tokyo&akey=18cce8ec1fb2982a4e11dd6b1b3efa36'
+  })
+    .then(res => {
+      const tokyoData = res.data.points.filter(point => point.name === '東京都心')[0]
+      const currentTokyoTemp = tokyoData.temp
+      return currentTokyoTemp
     })
-        .then(res => {
-            const tokyoData = res.data.points.filter(point => point.name === '東京都心')[0]
-            const currentTokyoTemp = tokyoData.temp
-            return currentTokyoTemp
-        })
-        .catch(err => {return err.response})
+    .catch(err => {return err.response})
 }
 
 async function getTomorrowPrecipitation() {
-    return axios.get('https://weather.biglobe.ne.jp/smart/data/areaTodayWeather.json')
-        .then(res => {
-            const tokyoData = res.data.weathers.filter(weather => {
-                return weather.prefectureName === '東京都' && weather.subPrefectureName === '東京地方'
-            })[0]
-            return tokyoData.precipitation
-        })
-        .catch(err => {
-            console.log(err.response.data)
-            return false
-        })
+  return axios.get('https://weather.biglobe.ne.jp/smart/data/areaTodayWeather.json')
+    .then(res => {
+      const tokyoData = res.data.weathers.filter(weather => {
+        return weather.prefectureName === '東京都' && weather.subPrefectureName === '東京地方'
+      })[0]
+      return tokyoData.precipitation
+    })
+    .catch(err => {
+      console.log(err.response.data)
+      return false
+    })
 }
 
 module.exports.getCurrentTemp = getCurrentTemp
